b72303c3d9a3ffc2ae085059eb2edfea48f1ef56
somlin <somlin.nayak@gmail.com>
2016-11-10 16:13:29 +0530

    Exploratory Analysis - Assignment 1- commit 2

--- plot4.R
+++ plot4.R
@@ -8,11 +8,11 @@ consum.data <- read.table("household_power_consumption.txt",header=TRUE,
 colnames( consum.data ) <- unlist(header)
 
 #Convert the data in Date field to class Date with as.Date()
-consum.data$Date <- strptime(paste(consum.data1$Date,consum.data1$Time),
+consum.data$Date <- strptime(paste(consum.data$Date,consum.data$Time),
                              format="%d/%m/%Y %H:%M:%S")
 
 # Get all the data for 2 days ( 2007-02-01 to 2007-02-02 )
-final.data <- subset(consum.data1,Date < "2007-02-03 00:00:00")
+final.data <- subset(consum.data,Date < "2007-02-03 00:00:00")
 
 #Generate the plot and save it to the disk
 png(filename="plot4.png")
